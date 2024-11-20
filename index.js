ec1ec148c1ed00ab224521066ac9fd19f615931f
saylekxd <adamrojek@icloud.com>
2024-11-20 14:43:47 +0100

    secret project finished edit1

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ app.get("/", async (req, res) => {
     try {
         const result = await axios.get(API_URL + "/random")
         res.render("index.ejs", {
-            secret: JSON.stringify(result.data.secret),
-            user: JSON.stringify(result.data.username)
+            secret: result.data.secret,
+            user: result.data.username
         })
     } catch(error){
         res.render("index.ejs", {content: JSON.stringify(error.response.data)});
       }
 })
+
 
 
 app.listen(port, () => {
